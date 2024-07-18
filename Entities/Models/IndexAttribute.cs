c067849c7495a94bc4634b3b4d570e7254299d63
Juan <juanmurcia893@gmail.com>
2024-07-18 16:41:14 -0500

    feat(Test): Add Method UpdateAsync() to Program - ConsoleAppTestDal

---
+++ Entities/Models/IndexAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace DAL.Models
+{
+    internal class IndexAttribute : Attribute
+    {
+        private string v;
+        private string name;
+
+        public IndexAttribute(string v, string Name)
+        {
+            this.v = v;
+            name = Name;
+        }
+
+        public string Name { get; set; }
+    }
+}
