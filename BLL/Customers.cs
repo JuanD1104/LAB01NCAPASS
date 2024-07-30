48db415705f2cee9d7ab4513e35e2e237eb5a7ae
Juan <juanmurcia893@gmail.com>
2024-07-30 14:45:27 -0500

    feat(bcak-end)Created branch BLL, Customers.cls, Exceptions and Methods for customers

---
+++ BLL/Customers.cs
@@ -0,0 +1,74 @@
+﻿using DAL;
+using DAL.Models;
+using Entities;
+using BLL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Customers
+    {
+        public async Task<Customer> CreateAsync(Customer customer)
+        {
+            Customer customerResult = null;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                //Buscar si el nombre del cliente existe
+                Customers customerSearch = await repository.RetreiveAsync<Customers>(c => c.FirstName == customer.FirstName);
+                if (customerSearch != null)
+                {
+                    //No existre, podemos Crearlo
+                    customerResult = await repository.CreateAsync(customer);
+                }
+                else 
+                {
+                    CustomerExceptions.ThrowCustomerAlreadyExistsException(customerSearch.FirstName, customerSearch.LastName);
+                }
+            }
+            return customerResult;
+
+        }
+
+        public async Task<List<Customer>> RetreiveByIDAsync(int id) 
+        {
+            Customer result = null;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                Customer customer = await repository.RetreiveAsync<Customer>(c => c.Id == id);
+
+                //Checkc if cuustomer was found
+                if (customer == null) 
+                {
+                    CustomerExceptions.ThrowInvalidCustomerDataException(id);
+                }
+                return customer;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Customer customer) 
+        {
+
+            bool Result = false;
+            using (var repository = RepositoryFactory.CreateRepository()) 
+            {
+                Customer customerSearch = await RepositoryFactory.RetrieveAsync<Customer>(c => c.FirstName == customer.FirstName && c.Id != customer.Id);
+                if (customerSearch == null) 
+                {
+                    //No existe
+                    Result = await repository.UpdateAsync(customer);
+                }
+                else
+                {
+                    CustomerExceptions.ThrowCustomerAlreadyExistsException(customerSearch.FirstName, customerSearch.LastName);
+                }
+            }
+            return Result;
+               
+        }
+    }
+}
---
+++ BLL/Exceptions/CustomersExceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BLL
+{
+    public class CustomerExceptions : Exception
+    {
+
+
+        private CustomerExceptions(string message) : base(message)
+        {
+        }
+        public static void ThrowCustomerAlreadyException(string firstName, string lastName)
+        {
+            throw new CustomerExceptions($"A client with the name already exist{firstName}{lastName}.");
+        }
+
+        public static void ThrowInvalidCustomerDataException(string message)
+        {
+            throw new CustomerExceptions(message);
+        }
+
+        internal static void ThrowCustomerAlreadyExistsException(object firstName, object lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void ThrowCustomerAlreadyExitsException(object firstName, object lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
